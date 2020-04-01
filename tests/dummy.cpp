2e6512b8029760bc3e9315beca4d0e41da49f0cb
Dominik Linn <dominik.linn@itwm.fraunhofer.de>
2020-04-01 23:04:34 +0200

    Fixed accidental deletion in vector_slice.hpp. TODO: Add tests

--- tests/dummy.cpp
+++ tests/dummy.cpp
@@ -1,4 +1,5 @@
 #include <cpputility/containers/storage_vector.hpp>
+#include <cpputility/containers/vector_slice.hpp>
 #include <cpputility/containers/vector_view.hpp>
 #include <iostream>
 #include <typeinfo>
@@ -8,20 +9,24 @@ int main(int, char **)
 {
     cpputility::StorageVector<double> vs;
 
-    std::vector<double> v{1.0, 2.0, 3.0};
+    std::vector<double> v{1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
 
     for (auto val : v) {
         vs.emplace_back(std::make_unique<double>(val));
     }
-    auto vv = cpputility::VectorView(vs);
+    auto vsl = cpputility::slice(vs, 1, vs.size(), 2);
 
-    std::cout << typeid(vv).name() << std::endl;
-    std::cout << vv[0] << std::endl;
-    for (auto &val : vv) {
-        val += 3;
+    auto vcsl = cpputility::slice(vs, 0, vs.size(), 2);
+
+    for (auto &val : vsl) {
+        val = 0;
     }
 
     for (auto &val : vs) {
+        std::cout << val << std::endl;
+    }
+
+    for (auto &val : vcsl) {
         std::cout << val << std::endl;
     }
 
